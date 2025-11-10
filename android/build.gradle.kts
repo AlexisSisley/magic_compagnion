efaa5fb9d3715937b33ba48d8d60375e5caa85dc
AlexisSisley <alexis.bourdon@sisley.fr>
2025-11-10 21:21:07 +0100

    fix ci

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,20 +3,6 @@ allprojects {
         google()
         mavenCentral()
     }
-
-    // --- AJOUTEZ CETTE LOGIQUE ICI ---
-    // On configure les modules Android (app ou library) dès que
-    // le plugin est appliqué. 'withId' gère le cycle de vie pour nous.
-    project.plugins.withId("com.android.application") {
-        project.extensions.getByType<com.android.build.api.dsl.CommonExtension<*, *, *, *, *, *>>().apply {
-            compileSdk = 34
-        }
-    }
-    project.plugins.withId("com.android.library") {
-        project.extensions.getByType<com.android.build.api.dsl.CommonExtension<*, *, *, *, *, *>>().apply {
-            compileSdk = 34
-        }
-    }
 }
 
 val newBuildDir: Directory =
@@ -29,7 +15,9 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-
+subprojects {
+    project.evaluationDependsOn(":app")
+}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
