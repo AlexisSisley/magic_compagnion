35e8df64912e07f839273158e56ece3e822c03e0
AlexisSisley <alexis.bourdon@sisley.fr>
2025-11-10 19:26:42 +0100

    fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,15 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    // Force tous les sous-projets (y compris les plugins défaillants)
+    // à utiliser une version de compileSdk qui inclut 'lStar'.
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.application") || project.plugins.hasPlugin("com.android.library")) {
+            project.extensions.findByType<com.android.build.api.dsl.BaseExtension>()?.apply {
+                compileSdk = 34 // Force 34 pour tout le monde
+            }
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
